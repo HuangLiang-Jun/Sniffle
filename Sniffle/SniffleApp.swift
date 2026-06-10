d76e71c9d89e8a5d0a9dde8a3aa2688955007f2d
Victor <bboydais@gmail.com>
2026-06-10 17:21:24 +0800

    Initial Commit

---
+++ Sniffle/SniffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SniffleApp.swift
+//  Sniffle
+//
+//  Created by Victor on 2026/6/10.
+//
+
+import SwiftUI
+
+@main
+struct SniffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
